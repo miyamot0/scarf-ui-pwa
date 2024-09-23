7f18447d2df839abc0ed03e83a4e66c9ebb97f9e
shawn gilroy <shawnpgilroy@gmail.com>
2024-09-23 07:55:53 -0400

    Add in shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@/styles/globals.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='container'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
