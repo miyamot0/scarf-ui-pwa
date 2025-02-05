4bb48f9b73f9558d9268e3f00f50f08d6538f808
Shawn Gilroy <shawnpgilroy@gmail.com>
2025-02-05 12:17:16 -0600

    Add in fixes for typos, add in older archives

--- src/questions/convert_banks.ts
+++ src/questions/convert_banks.ts
@@ -1,61 +1,64 @@
 // Note: Simpler helper class to manage questions
 
-let csvToJson = require('convert-csv-to-json')
+import csvToJson from 'convert-csv-to-json';
+
+//const csvToJson = require('convert-csv-to-json');
 
 try {
-    console.log('Converting Question Banks...')
-    console.log(`Working Directory: ${process.cwd()}`)
+  process.chdir('src');
 
-    let fileInputName = './questions/banks/external_validity_questions.csv'
-    let fileOutputName =
-        './questions/generated/external_validity_questions.json'
+  console.log('Converting Question Banks...');
+  console.log(`Working Directory: ${process.cwd()}`);
 
-    console.log(`Converting ${fileInputName} to ${fileOutputName}`)
-    csvToJson
-        .fieldDelimiter(',')
-        .utf8Encoding()
-        .supportQuotedField(true)
-        .generateJsonFileFromCsv(fileInputName, fileOutputName)
+  let fileInputName = './questions/banks/external_validity_questions.csv';
+  let fileOutputName = './questions/generated/external_validity_questions.json';
 
-    fileInputName = './questions/banks/internal_validity_questions.csv'
-    fileOutputName = './questions/generated/internal_validity_questions.json'
+  console.log(`Converting ${fileInputName} to ${fileOutputName}`);
+  csvToJson
+    .fieldDelimiter(',')
+    .utf8Encoding()
+    .supportQuotedField(true)
+    .generateJsonFileFromCsv(fileInputName, fileOutputName);
 
-    console.log(`Converting ${fileInputName} to ${fileOutputName}`)
-    csvToJson
-        .fieldDelimiter(',')
-        .utf8Encoding()
-        .supportQuotedField(true)
-        .generateJsonFileFromCsv(fileInputName, fileOutputName)
+  fileInputName = './questions/banks/internal_validity_questions.csv';
+  fileOutputName = './questions/generated/internal_validity_questions.json';
 
-    fileInputName = './questions/banks/outcomes_questions.csv'
-    fileOutputName = './questions/generated/outcomes_questions.json'
+  console.log(`Converting ${fileInputName} to ${fileOutputName}`);
+  csvToJson
+    .fieldDelimiter(',')
+    .utf8Encoding()
+    .supportQuotedField(true)
+    .generateJsonFileFromCsv(fileInputName, fileOutputName);
 
-    console.log(`Converting ${fileInputName} to ${fileOutputName}`)
-    csvToJson
-        .fieldDelimiter(',')
-        .utf8Encoding()
-        .supportQuotedField(true)
-        .generateJsonFileFromCsv(fileInputName, fileOutputName)
+  fileInputName = './questions/banks/outcomes_questions.csv';
+  fileOutputName = './questions/generated/outcomes_questions.json';
 
-    fileInputName = './questions/banks/reporting_questions.csv'
-    fileOutputName = './questions/generated/reporting_questions.json'
+  console.log(`Converting ${fileInputName} to ${fileOutputName}`);
+  csvToJson
+    .fieldDelimiter(',')
+    .utf8Encoding()
+    .supportQuotedField(true)
+    .generateJsonFileFromCsv(fileInputName, fileOutputName);
 
-    console.log(`Converting ${fileInputName} to ${fileOutputName}`)
-    csvToJson
-        .fieldDelimiter(',')
-        .utf8Encoding()
-        .supportQuotedField(true)
-        .generateJsonFileFromCsv(fileInputName, fileOutputName)
+  fileInputName = './questions/banks/reporting_questions.csv';
+  fileOutputName = './questions/generated/reporting_questions.json';
 
-    fileInputName = './questions/banks/planning_questions.csv'
-    fileOutputName = './questions/generated/planning_questions.json'
+  console.log(`Converting ${fileInputName} to ${fileOutputName}`);
+  csvToJson
+    .fieldDelimiter(',')
+    .utf8Encoding()
+    .supportQuotedField(true)
+    .generateJsonFileFromCsv(fileInputName, fileOutputName);
 
-    console.log(`Converting ${fileInputName} to ${fileOutputName}`)
-    csvToJson
-        .fieldDelimiter(',')
-        .utf8Encoding()
-        .supportQuotedField(true)
-        .generateJsonFileFromCsv(fileInputName, fileOutputName)
+  fileInputName = './questions/banks/planning_questions.csv';
+  fileOutputName = './questions/generated/planning_questions.json';
+
+  console.log(`Converting ${fileInputName} to ${fileOutputName}`);
+  csvToJson
+    .fieldDelimiter(',')
+    .utf8Encoding()
+    .supportQuotedField(true)
+    .generateJsonFileFromCsv(fileInputName, fileOutputName);
 } catch (error) {
-    console.log('Error converting external validity questions: ', error)
+  console.log('Error converting external validity questions: ', error);
 }
