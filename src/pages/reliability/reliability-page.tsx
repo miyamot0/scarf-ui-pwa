5abbbc2673491b0d1af4791ac753862ddd239237
Shawn Gilroy <shawnpgilroy@gmail.com>
2025-05-11 08:25:28 -0500

    Add instructions for Reliability page

--- src/pages/reliability/reliability-page.tsx
+++ src/pages/reliability/reliability-page.tsx
@@ -15,10 +15,34 @@ export function ReliabilityPage() {
         <div className="flex flex-col gap-y-4 w-full">
           <Card>
             <CardHeader>
-              <CardTitle>Reliability Check</CardTitle>
-              <CardDescription>Compute user agreement and view correspondence</CardDescription>
+              <CardTitle>Reliability Assessment and Evaluation</CardTitle>
+              <CardDescription>Compute user agreement and work toward consensus</CardDescription>
             </CardHeader>
             <CardContent className="flex flex-col gap-y-4">
+              <div className="flex flex-col gap-y-2">
+                <p>
+                  This page is designed to compare TWO SCARF data sets (i.e., Primary and Reliability coding) and
+                  support reaching 100% agreement across both coders. Instances of diagreement will be highlighted in{' '}
+                  <span className="text-red-700 font-bold">RED</span> to prompt discussion and resolve disagreements.
+                  Instructions for using this page are provided below and in the information section of the webapp.
+                </p>
+                <ol className="list-decimal list-inside">
+                  <li>Load Primary and Reliability data sets using the associated buttons below.</li>
+                  <li>
+                    Inspect the resulting table for areas of disagreement indicated by a{' '}
+                    <span className="text-red-700 font-bold">RED</span> color.
+                  </li>
+                  <li>
+                    Clicking on the cells with disagreement will open a dialog that will allow you to update BOTH
+                    datasets with the value the pair (or tiebreaker) feels is most correct for the field and
+                    demonstration.
+                  </li>
+                  <li>
+                    Once coded to complete agreement, both of the updated datasets (Primary and Reliability) can be
+                    downloaded using fields near the initial upload buttons.
+                  </li>
+                </ol>
+              </div>
               <div className="flex flex-col lg:flex-row gap-y-4 gap-x-4">
                 <DataInputPanelWithRef coderType={'Primary'} />
 
