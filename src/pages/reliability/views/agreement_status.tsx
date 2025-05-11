8a70fb5f113a72b030067a40fd20598762291f6e
Shawn Gilroy <shawnpgilroy@gmail.com>
2025-05-11 09:10:20 -0500

    Cleanup Reli Page for now

--- src/pages/reliability/views/agreement_status.tsx
+++ src/pages/reliability/views/agreement_status.tsx
@@ -5,6 +5,22 @@ import { BuildColumns } from '@/components/tables/reli/study_reli_columns';
 import { StudyObjectPair } from '@/questions/types/QuestionTypes';
 import { ConsensusStateContext } from '../context/consensus-provider';
 import { useContext } from 'react';
+import { cn } from '@/lib/utils';
+
+function StyledSpan({ Text, Number }: { Text: string; Number: number }) {
+  return (
+    <span>
+      {`${Text} = `}
+      <span
+        className={cn('', {
+          'text-red-700 font-bold': Number < 100,
+        })}
+      >
+        {Number}
+      </span>
+    </span>
+  );
+}
 
 export const AgreementStatus = () => {
   const { context } = useContext(ConsensusStateContext);
@@ -26,18 +42,21 @@ export const AgreementStatus = () => {
       <>
         <Card>
           <CardHeader>
-            <CardTitle>Agreement Status</CardTitle>
+            <CardTitle>State of Rater Agreement</CardTitle>
             <CardDescription>Information on agreement is provided in the space below</CardDescription>
           </CardHeader>
           <CardContent className="flex flex-col gap-y-4">
             <div className="flex flex-col gap-y-0">
               <p>{`Total Percentage Overall Agreement: ${agreement.TotalPercent}`}</p>
-              <p>
-                {`Internal Validity = ${agreement.IV.Percent}, `}
-                {`External Validity = ${agreement.EV.Percent}, `}
-                {`Reporting = ${agreement.Reporting.Percent}, `}
-                {`Outcomes = ${agreement.Outcomes.Percent}`}
-              </p>
+              <div className="">
+                <StyledSpan Text="Internal Validity" Number={agreement.IV.Percent} />
+                {'; '}
+                <StyledSpan Text="External Validity" Number={agreement.EV.Percent} />
+                {'; '}
+                <StyledSpan Text="Reporting" Number={agreement.Reporting.Percent} />
+                {'; '}
+                <StyledSpan Text="Outcomes" Number={agreement.Outcomes.Percent} />
+              </div>
             </div>
 
             <StudyReliDataTable columns={built_columns} data={study_pairs} />
